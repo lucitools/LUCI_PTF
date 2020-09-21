f06bb1ea6b5dc052b8e53d475d65ae63e9bdeaf9
benaviru <Rubianca.Benavidez@vuw.ac.nz>
2020-09-22 10:55:56 +1200

    Additional help text and system checks

--- lib/common.py
+++ lib/common.py
@@ -138,6 +138,21 @@ def runSystemChecks(folder=None, rerun=False):
         log.warning("Disk containing scratch workspace has less than " + str(freeSpaceGb) + "Gb free space. This may cause this tool to fail.")
 
 
+def getFreeDiskSpaceGb(dirname):
+
+    """Return folder/drive free space (in megabytes)."""
+
+    import ctypes
+    import platform
+
+    if platform.system() == 'Windows':
+        free_bytes = ctypes.c_ulonglong(0)
+        ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(dirname), None, None, ctypes.pointer(free_bytes))
+        return free_bytes.value / 1024 / 1024 / 1024
+    else:
+        st = os.statvfs(dirname)
+        return st.f_bavail * st.f_frsize / 1024 / 1024 / 1024
+
 def paramsAsText(params):
 
     paramsText = []
